7f9c304896135fe7541874ecb0c986a1c6d064b3
Fabio <fabioguieiro@gmail.com>
2020-07-29 22:41:34 -0300

    card desenhado

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,52 @@
 
-import React from 'react'
-import { DrawedCard } from './CardStyles'
+import React, { Component } from 'react'
+import { DrawedCard, Icon, TurnText, ActiveSide, Content, Circle, Triangle, Wave } from './CardStyles'
+import turnIcon from '../assets/icons/turn.png'
+import triangle from '../assets/icons/triangle.svg'
+import IconWave from '../assets/icons/wave.svg'
+import ReactCardFlip from 'react-card-flip';
 
-const card = (props) => (
-    <DrawedCard>
-        
-    </DrawedCard>
-)
 
-export default card;
+
+class Card extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isFlipped: false
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(e) {
+        e.preventDefault();
+        this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+    }
+    render() {
+        return (
+            <ReactCardFlip isFlipped={this.state.isFlipped} style={{position: "relative", top: "20%" }}>
+
+                <DrawedCard  onClick={this.handleClick}>
+
+                    <Icon src={turnIcon} ></Icon>
+                    <TurnText>Virar Carta</TurnText>
+                    <ActiveSide>A<span>\B</span></ActiveSide>
+                    <Content>No problem, do you have your passport with you?</Content>
+                    <Circle><Triangle src={triangle}></Triangle></Circle>
+                    <Wave src={IconWave}></Wave>
+
+                </DrawedCard>
+                <DrawedCard  onClick={this.handleClick} >
+
+                    <Icon src={turnIcon} ></Icon>
+                    <TurnText>Virar Carta</TurnText>
+                    <ActiveSide><span>A\</span>B</ActiveSide>
+                    <Content>Costas do cartao</Content>
+                    <Circle><Triangle src={triangle}></Triangle></Circle>
+                    <Wave src={IconWave}></Wave>
+
+                </DrawedCard>
+            </ReactCardFlip>
+        )
+    }
+}
+
+export default Card;
--- src/components/CardStyles.js
+++ src/components/CardStyles.js
@@ -14,3 +14,78 @@ export const DrawedCard = styled.div`
     box-shadow: 0px 1px 2px #32788D;
     
 `;
+
+export const Icon = styled.img`
+    position: relative;
+    top: 20px;
+    left: 20px;    
+    width: 15px;
+    heigth: 15px;
+`;
+
+export const TurnText = styled.p`
+    @import url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;500;600&display=swap');    
+    position: relative;
+    width: 100px;
+    margin: initial;
+    left: 45px;
+    font-family: 'Fira Sans',sans-serif;
+    font-size: 12px;
+    font-weight: 600;
+    letter-spacing: 0.7px;
+    color: #F24556;
+`;
+
+export const ActiveSide = styled.p`
+    position: relative;
+    bottom: 30px;
+    text-align: right;
+    right: 30px;
+    font-weight: 600;
+    font-family: 'Fira Sans',sans-serif;
+    letter-spacing: 5px;
+    color: #000000;
+    opacity: 1;
+    span{
+        color: #c2c2c2;
+    }
+`;
+
+export const Content = styled.p`
+    @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@500;600;700&display=swap');
+    width: 600px;
+    height: 95px;
+    padding: 20px;
+    font-weight: 600;
+    font-size: 40px;
+    text-align: left;
+    font-family: 'IBM Plex Serif',serif;
+    color: #F24556;
+    opacity: 1;
+`;
+
+export const Circle  = styled.div`
+    position: relative;
+    top: -45px;
+    right: 35px;
+    float: right;
+    width: 74px;
+    height: 74px;
+    border-radius: 50px;
+    background-color: #00AEE0;
+`;
+
+export const Triangle  = styled.img`
+    margin-top: 23px;
+    margin-left: 28px;
+`;
+
+export const Wave  = styled.img`
+    position: relative;
+    left: 20px;
+    bottom: 30px;
+    width: 400px;
+    height: 74px;
+    border-radius: 50px;
+`;
+
